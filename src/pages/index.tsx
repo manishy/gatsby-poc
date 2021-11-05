5900b2479b8bc6ef8d8a4c09856cfeb6593fdfc4
Manish <manishy@thoughtworks.com>
2021-11-05 16:11:34 +0530

    Add styling in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import Books from "../components/Books";
+import './index.scss';
 
 export const PureIndexPage = ({ data }) => (
-  <div>
+  <div className="main">
     <div>
       <div>{<Books books={data.allContentfulBook.edges} />}</div>
     </div>
