4b04c6e0362aaba9beac7fa8bff84410168eeb19
Manish <manishy@thoughtworks.com>
2021-11-05 17:52:11 +0530

    Refactor Book to include image and description

--- src/templates/book.tsx
+++ src/templates/book.tsx
@@ -1,21 +1,38 @@
 import React from "react";
 import { graphql } from "gatsby";
+import "./book.scss";
 
 export const query = graphql`
   query($slug: String!) {
     contentfulBook(slug: { eq: $slug }) {
       name
       author
+      description {
+        description
+      }
+      image {
+        file {
+          url
+          fileName
+          contentType
+        }
+      }
     }
   }
 `;
 
-const Book = (props) => {
-  const book = props.data.contentfulBook;
+const Book = ({ data }) => {
+  const book = data.contentfulBook;
+  const { name, author, description, image } = book;
+
   return (
-    <div>
-      <h2>{book.name}</h2>
-      <span>{book.author}</span>
+    <div className="book">
+      <div className="book__details">
+        <h2>{name}</h2>
+        <span>{author}</span>
+        <p> {description && description.description} </p>
+      </div>
+      <div>{image && <img src={`https://${image.file.url}`} alt="image not found" />}</div>
     </div>
   );
 };
